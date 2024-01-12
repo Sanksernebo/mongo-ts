09d6432d26e4dbd3e72dbceb83d9a07d9e134fe7
Sanksernebo <karlmarkus.sangernebo@gmail.com>
2024-01-12 12:53:14 +0200

    Created example online store database with MongoDB

--- index.ts
+++ index.ts
@@ -2,12 +2,18 @@ import express, { Express, Request, Response } from "express";
 import commentController from "./controllers/commentController";
 import articleController from "./controllers/articleController";
 import authorController from "./controllers/authorController";
+import userController from './controllers/UserController';
+import categoryController from './controllers/CategoryController';
+import productController from './controllers/ProductController';
+import cartProductController from './controllers/CartProductController';
+import orderController from './controllers/OrderController';
 
 import mongoose from "mongoose";
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb+srv://Sanks:RFHyvxXT6NKVhaaL@mongo-ts.weyyukl.mongodb.net/");
 const database = mongoose.connection;
@@ -30,7 +36,11 @@ app.use('/', commentController);
 app.use('/', articleController);
 app.use('/', authorController);
 
-
+app.use('/', userController);
+app.use('/', categoryController);
+app.use('/', productController);
+app.use('/', cartProductController);
+app.use('/', orderController);
 
 
 app.listen(3000,() => {
